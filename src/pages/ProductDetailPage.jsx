23c0926ed06ab25a25514579fe56fe81901f2c40
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2023-08-12 21:41:38 +0530

    made visual changes to product Description

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -10,15 +10,17 @@ function ProductDetailPage() {
     return (
         <div className='Inventory DashBoardPage w-screen flex'>
             <LeftSideBar />
-            <div className="dashboard w-full p-10 bg-[#f1f1f1] flex flex-wrap gap-8 ">
-                <h1>{Product.name}</h1>
-                <img src={Product.imageUrl} alt="" />
-                <p>Stock: {Product.stock}</p>
-                <p>Supplier: {Product.supplier}</p>
-                <p>Department: {Product.department}</p>
-                <p>Delivered: {Product.delivered}</p>
-                <p>SKU: {Product.sku}</p>
-                <p>Description: {Product.description}</p>
+            <div className="dashboard w-full p-10 bg-[#f1f1f1] flex flex-col gap-5 ">
+                <h1 className="text-2xl">{Product.name}</h1>
+                <div className="content bg-white p-10 rounded=lg shadow-md flex-col flex gap-2">
+                    <img className="h-[10rem] w-[10rem]" src={Product.imageUrl} alt="" />
+                    <p>Stock: {Product.stock}</p>
+                    <p>Supplier: {Product.supplier}</p>
+                    <p>Department: {Product.department}</p>
+                    <p>Delivered: {Product.delivered}</p>
+                    <p>SKU: {Product.sku}</p>
+                    <p>Description: {Product.description}</p>
+                </div>
             </div>
         </div>
     )
